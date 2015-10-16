2a991b29a079d7e726d5c1e4783fbb4df6591eca
roman2013 <romanyu827@gmail.com>
2015-10-16 18:09:12 +0800

    Add tableview and search bar.

--- SwiftLearner/ViewController.swift
+++ SwiftLearner/ViewController.swift
@@ -10,23 +10,60 @@ import UIKit
 import PureLayout
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    let cellIdentifier = "cell"
+
+    var tableView : UITableView = UITableView ()
+    
+    var searchBar : UISearchBar = UISearchBar(frame: CGRectMake(0, 0, 200, 20))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Hello, world!")
+        tableView = UITableView(frame: UIScreen.mainScreen().bounds, style: UITableViewStyle.Plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        self.view.addSubview(tableView)
         
-        var myVar = 42
-        
-        myVar = 43
+        searchBar.placeholder = "Your placeholder"
+        searchBar.delegate = self
+        let leftNavBarButton = UIBarButtonItem(customView:searchBar)
+        self.navigationItem.leftBarButtonItem = leftNavBarButton
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Layout
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+        tableView.autoPinEdge(.Top, toEdge: .Top, ofView: self.view, withOffset: 50)
+        tableView.autoPinEdgesToSuperviewEdgesWithInsets(UIEdgeInsetsZero, excludingEdge: .Top)
+    }
 
-
+    // MARK: UITableViewDataDelegate
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = "Hello world!"
+        
+        return cell
+    }
+    
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        return true
+    }
 }
 
